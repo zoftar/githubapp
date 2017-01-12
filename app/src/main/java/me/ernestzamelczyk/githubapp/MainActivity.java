790ea5e89fe93d5e20e33cfb7e704f8607c93789
ernest <ernest.zamelczyk@gmail.com>
2017-01-12 19:08:33 +0100

    lala added

--- app/src/main/java/me/ernestzamelczyk/githubapp/MainActivity.java
+++ app/src/main/java/me/ernestzamelczyk/githubapp/MainActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int lala;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.lala = 5;
+    }
+
+    public int getLala() {
+        return lala;
     }
 }
